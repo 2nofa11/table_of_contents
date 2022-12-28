bfb6eba3a05cb1601184b5578feb0c72f3fbaed2
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-28 09:26:59 +0900

    chore:使う関数の変更

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { ref } from "vue";
-import { fetchOpenDB } from "./module/openDB/openDB";
+import { extractTextContents } from "./module/openDB/openDB";
 </script>
 
 <template>
-  <button @click="fetchOpenDB">おす</button>
+  <button @click="extractTextContents">おす</button>
   <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
