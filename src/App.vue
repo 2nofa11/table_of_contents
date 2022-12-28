cfd7c8f6090a03ff94654f9daaccd33b88c4bc40
2nofa11 <aboubthethirdkimi@icloud.com>
2022-12-28 09:24:25 +0900

    feat:tailwindの導入

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,7 @@ import { fetchOpenDB } from "./module/openDB/openDB";
 
 <template>
   <button @click="fetchOpenDB">おす</button>
-  でてない
+  <h1 class="text-3xl font-bold underline">Hello world!</h1>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
